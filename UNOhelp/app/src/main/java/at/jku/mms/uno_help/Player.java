432eb3fda3fdeb64c98fa82e6d9444119d5fc143
MrMinemeet <alexander.voglsperger@gmail.com>
2021-05-05 16:27:04 +0200

    Update

--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
@@ -3,10 +3,6 @@ package at.jku.mms.uno_help;
 import java.util.ArrayList;
 
 public class Player {
-    private final String name;
+    private String name;
     private ArrayList<Card> currentDeck;
-
-    public Player(String name) {
-        this.name = name;
-    }
 }

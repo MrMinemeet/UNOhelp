4d52cf3015b2286475d307e0a5b22eab8d773ef9
stiegxn <s.stiegler17@gmail.com>
2021-05-05 16:47:05 +0200

    setName
    setName fragt nach Name

--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
@@ -1,8 +1,31 @@
 package at.jku.mms.uno_help;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class Player {
     private String name;
     private ArrayList<Card> currentDeck;
+
+    public void setName(Context c){
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Add a new player")
+                .setMessage("What is the name of the player?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = String.valueOf(taskEditText.getText());
+                        Toast.makeText(c, "Name: " + name, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
 }

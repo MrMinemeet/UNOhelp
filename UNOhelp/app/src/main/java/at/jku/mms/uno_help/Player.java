9d224dc5e86c5b6b5e9c2afb15df433055389bc7
MrMinemeet <alexander.voglsperger@gmail.com>
2021-05-13 22:32:10 +0200

    Added Multi-Language for Username Pop-Up

--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/Player.java
@@ -15,17 +15,17 @@ public class Player {
     public void setName(Context c) {
         final EditText taskEditText = new EditText(c);
         AlertDialog dialog = new AlertDialog.Builder(c)
-                .setTitle("Add a new player")
-                .setMessage("What is the name of the player?")
+                .setTitle(R.string.player_name_title)
+                .setMessage(R.string.player_name_text)
                 .setView(taskEditText)
-                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         String name = String.valueOf(taskEditText.getText());
                         Toast.makeText(c, "Name: " + name, Toast.LENGTH_SHORT).show();
                     }
                 })
-                .setNegativeButton("Cancel", null)
+                .setNegativeButton(R.string.cancel, null)
                 .create();
         dialog.show();
     }

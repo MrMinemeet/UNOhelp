32408fd439c65ace459f13df2cc514e496481b8a
MrMinemeet <alexander.voglsperger@gmail.com>
2021-05-13 22:33:51 +0200

    Moved permissions request so splash screen
    Camera permissions request is now done in splash screen.
    If Successfull the timer for switching to MainActivity is started

--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/MainActivity.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/MainActivity.java
@@ -1,14 +1,10 @@
 package at.jku.mms.uno_help;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
@@ -21,8 +17,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private CodeScanner codeScanner;
-    private static final int RC_PERMISSION = 10;
-    private boolean permissionsGranted;
 
     private static final Player player = new Player();
 
@@ -31,48 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Check if android version is higher than 23
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Check if permissions have been granted
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                // Ask for permissions
-                permissionsGranted = false;
-                requestPermissions(new String[] {Manifest.permission.CAMERA}, RC_PERMISSION);
-            } else {
-                // Permissions where already granted
-                permissionsGranted = true;
-            }
-        } else {
-            // No need to ask for permissions explicit
-            permissionsGranted = true;
-        }
-
-        if(permissionsGranted) {
-            // Try to get CodeScannerView from activity_main
-            CodeScannerView scannerView = findViewById(R.id.scanner_view);
-
-            // Setup code scanner
-            codeScanner = new CodeScanner(this, scannerView);
-
-            // Keeps scanning after a code was found
-            codeScanner.setScanMode(ScanMode.CONTINUOUS);
-
-            // Set callback when a code was read/decoded
-            codeScanner.setDecodeCallback(new DecodeCallback() {
-                @Override
-                public void onDecoded(@NonNull final Result result) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Set decoded text into TextView
-                            TextView tv = findViewById(R.id.text_result);
-                            tv.setText(result.getText());
-                        }
-                    });
-                }
-            });
-        }
+        // Create pop-up for name input
         player.setName(this);
+
+        // Try to get CodeScannerView from activity_main
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+
+        // Setup code scanner
+        codeScanner = new CodeScanner(this, scannerView);
+
+        // Keeps scanning after a code was found
+        codeScanner.setScanMode(ScanMode.CONTINUOUS);
+
+        // Set callback when a code was read/decoded
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Set decoded text into TextView
+                        TextView tv = findViewById(R.id.text_result);
+                        tv.setText(result.getText());
+                    }
+                });
+            }
+        });
     }
 
     @Override
--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/SplashScreen.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/SplashScreen.java
@@ -1,28 +1,53 @@
 package at.jku.mms.uno_help;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SplashScreen extends AppCompatActivity {
+    private static final int RC_PERMISSION = 10;
+    private boolean permissionsGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        // Basically start a timer that executes this code block after 3000ms
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Start new Intent for switching to main Activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
-
-                finish();
+        // Check if android version is higher than 23
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check if permissions have been granted
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // Ask for permissions
+                permissionsGranted = false;
+                requestPermissions(new String[] {Manifest.permission.CAMERA}, RC_PERMISSION);
+            } else {
+                // Permissions where already granted
+                permissionsGranted = true;
             }
-        }, 3000);
+        } else {
+            // No need to ask for permissions explicit
+            permissionsGranted = true;
+        }
+
+        if(permissionsGranted) {
+            // Permissions got granted
+
+            // Basically start a timer that executes this code block after 3000ms
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Start new Intent for switching to main Activity
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+
+                    finish();
+                }
+            }, 3000);
+        }
     }
 }

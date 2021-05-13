543ac881931a0639888118508aaf5871d40eb407
MrMinemeet <alexander.voglsperger@gmail.com>
2021-05-13 23:02:17 +0200

    Extended Card with functions and elements

--- UNOhelp/app/src/main/java/at/jku/mms/uno_help/Card.java
+++ UNOhelp/app/src/main/java/at/jku/mms/uno_help/Card.java
@@ -4,7 +4,42 @@ public class Card {
     /* Eigenschaften für Karte
         Bsp: Enun - Kartentyp
              String name - QR code
-
-
      */
+
+    private final CardType cardType;
+    private final CardColor cardColor;
+    private final String qrCodeName;
+
+    public Card(CardType type, CardColor color, String qrCodeName) {
+        this.cardType = type;
+        this.cardColor = color;
+        this.qrCodeName = qrCodeName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass() == this.getClass()) {
+            Card other = (Card)obj;
+            return other.getCardColor() == this.getCardColor() && other.getCardType() == this.getCardType();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Card Color: " + this.getCardColor() + " - Card Type: " + this.getCardType();
+    }
+
+    // Getter
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public CardColor getCardColor() {
+        return cardColor;
+    }
+
+    public String getQrCodeName() {
+        return qrCodeName;
+    }
 }
